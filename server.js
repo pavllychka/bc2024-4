ee52393cfba4c8a7fa9729c2b6783afa99c480f8
pavllychka bc2024-4 <spavlyk7@gmail.com>
2024-11-01 16:20:33 +0200

    додала 2 частину get

--- server.js
+++ server.js
@@ -1,44 +1,69 @@
-const http = require('http'); 
-const { Command } = require('commander'); 
-const fs = require('fs'); 
- 
-const program = new Command(); 
- 
-program 
-  .requiredOption('-h, --host <host>', 'Server host address') 
-  .requiredOption('-p, --port <port>', 'Server port') 
-  .requiredOption('-c, --cache <cachePath>', 'Cache directory path'); 
- 
-const args = process.argv.slice(2); 
- 
-if (!args.includes('-h') && !args.includes('--host') || 
-    !args.includes('-p') && !args.includes('--port') || 
-    !args.includes('-c') && !args.includes('--cache')) { 
-  console.log('Параметр не задано!'); 
-  process.exit(1); 
-} 
- 
-program.parse(process.argv); 
- 
-const { host, port, cache } = program.opts(); 
- 
- 
-if (isNaN(port) || port <= 0  || port > 65535) { 
-  console.log('Помилка: Порт має бути числом в діапазоні від 1 до 65535!'); 
-  process.exit(1); 
-} 
- 
- 
-if (!fs.existsSync(cache)) { 
-  console.log('Помилка: Кеш-каталог не існує!'); 
-  process.exit(1); 
-} 
- 
-const server = http.createServer((req, res) => { 
-  res.writeHead(200, { 'Content-Type': 'text/plain' }); 
-  res.end('bim bam boom  \n'); 
-}); 
- 
-server.listen(port, host, () => { 
-  console.log(`Сервер запущено  http://${host}:${port}`); 
+const http = require('http');
+const { Command } = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+const program = new Command();
+
+program
+  .requiredOption('-h, --host <host>', 'Server host address')
+  .requiredOption('-p, --port <port>', 'Server port')
+  .requiredOption('-c, --cache <cachePath>', 'Cache directory path');
+
+const args = process.argv.slice(2);
+
+if (!args.includes('-h') && !args.includes('--host') ||
+    !args.includes('-p') && !args.includes('--port') ||
+    !args.includes('-c') && !args.includes('--cache')) {
+  console.log('Параметр не задано!');
+  process.exit(1);
+}
+
+program.parse(process.argv);
+
+const { host, port, cache } = program.opts();
+
+if (isNaN(port) || port <= 0 || port > 65535) {
+  console.log('Помилка: Порт має бути числом в діапазоні від 1 до 65535!');
+  process.exit(1);
+}
+
+if (!fs.existsSync(cache)) {
+  console.log('Помилка: Кеш-каталог не існує!');
+  process.exit(1);
+}
+
+
+const get = (req, res) => {
+  const urlParts = req.url.split('/');
+  const httpCode = urlParts[urlParts.length - 1];
+  
+  const filePath = path.join(cache, `${httpCode}.jpg`);  
+
+  
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Image is not found  \n');
+      return;
+    }
+
+   
+    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+    const readStream = fs.createReadStream(filePath);
+    readStream.pipe(res);
+  });
+};
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    get(req, res);  
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Method is not allowed \n');
+  }
+});
+
+server.listen(port, host, () => {
+  console.log(`Сервер запущено http://${host}:${port}`);
 });
